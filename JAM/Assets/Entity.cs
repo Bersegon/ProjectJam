ef1c0ab3da239049a1511c028f1fe1af73a4feab
Bersegon <patrik@laczko.hu>
2022-03-29 20:48:08 +0200

    Entity

---
+++ JAM/Assets/Entity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class Entity : MonoBehaviour
+{
+    protected int hp;   //karakter hp-ja
+    public int HP{ get { return hp; } set { hp = value; } }
+    protected int maxhp;  //karakter maxhp-ja
+    public int MaxHP { get { return maxhp; } set { maxhp = value; } }
+
+    protected Rigidbody2D Body;
+    public Rigidbody2D getBody { get{ return Body; } }
+    
+}
+
+public class Player : Entity
+{   
+    public Player(Rigidbody2D P) { Body = P; }
+    public void Update()
+    {
+        float directionx = Input.GetAxisRaw("Horizontal");
+        float directiony = Input.GetAxisRaw("Vertical");
+        Body.velocity = new Vector2(directionx * 7f, Body.velocity.y);
+        Body.velocity = new Vector2(Body.velocity.x, directiony * 7f);
+    }
+}
+
+
+
--- JAM/Assets/movement.cs
+++ JAM/Assets/movement.cs
@@ -2,26 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime;
+
 public class movement : MonoBehaviour
 {
-    private Rigidbody2D Test;
+    Player jatekos;
     // Start is called before the first frame update
     void Start()
     {
-        Test = GetComponent<Rigidbody2D>();
+        jatekos = new Player(GetComponent<Rigidbody2D>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        float directionx = Input.GetAxisRaw("Horizontal");
-        float directiony = Input.GetAxisRaw("Vertical");
-        Test.velocity = new Vector2(directionx * 7f, Test.velocity.y);
-        Test.velocity = new Vector2(Test.velocity.x, directiony*7f);
-        if (Input.GetButtonDown("Jump"))
-        {
-            Test.position +=new Vector2(Random.Range(-3, 3),Random.Range(-3, 3));
-        }
-        
+        jatekos.Update();
     }
 }
