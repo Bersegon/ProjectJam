db0c8fe2e97c69c2c5ff05bcf38141885509ab07
Bersegon <patrik@laczko.hu>
2022-03-31 16:18:12 +0200

    Revert guns

---
+++ JAM/Assets/Playermanager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime;
+
+public class Playermanager : MonoBehaviour
+{
+   Player jatekos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        jatekos = new Player(GetComponent<Rigidbody2D>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jatekos.Update();
+        
+    }
+   
+    
+  
+}
